c206bbc587123192e53ace9a1737372316752e1f
Jens A. Koch <jakoch@web.de>
2025-03-14 20:50:43 +0100

    rename theme "jurasic" to "jurassic"

--- CMakeLists_resources.cmake
+++ CMakeLists_resources.cmake
@@ -2,7 +2,7 @@
 target_resources(hikogui
     ${CMAKE_CURRENT_SOURCE_DIR}/resources/hikogui_dark.theme.json
     ${CMAKE_CURRENT_SOURCE_DIR}/resources/hikogui_light.theme.json
-    ${CMAKE_CURRENT_SOURCE_DIR}/resources/jurasic.theme.json
+    ${CMAKE_CURRENT_SOURCE_DIR}/resources/jurassic.theme.json
     ${CMAKE_CURRENT_SOURCE_DIR}/resources/pink.theme.json
     ${CMAKE_CURRENT_SOURCE_DIR}/resources/elusiveicons-webfont.ttf
     ${CMAKE_CURRENT_SOURCE_DIR}/resources/hikogui_icons.ttf
